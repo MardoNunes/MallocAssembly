a4e9d6914caafcafd79d736fcf92badab070c4f8
MARDOQUEU FREIRE NUNES <mfn21@inf.ufpr.br>
2023-10-25 21:30:00 -0300

    libera memoria

--- malloc.s
+++ malloc.s
@@ -3,6 +3,7 @@
     TOPO_HEAP .quad 0
 .section .text
 .globl _start
+
 iniciaAlocador:
     push %rbp
     movq %rsp, %rbp
@@ -23,9 +24,24 @@ finalizaAlocador:
     movq %rsp, %rbp
 
     movq $12, %rax
-    movq INICIO_HEAP, %rdi
+    movq INICIO_HEAP, %rdi  # restauro a heap
     syscall
 
     popq %rbp
     ret
+
+liberaMem:
+    push %rbp
+    movq %rsp, %rbp
+    movq 16(%rbp), %r10 # movo o parametro para %r10, o parametro passado é o comeco do bloco
+
+    movq $0, -16(%r10)  #-16 é onde esta os bit de dirt, coloco zero para dizer q o bloco esta livre
+    movq -16(%r10), %rbx # movo o dirt para %rbx
+
+    popq %rbp
+    ret
+
+
+
+
     
